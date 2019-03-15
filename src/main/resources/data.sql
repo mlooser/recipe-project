2f06dd4ad33e9f0de99331854cff5a073fa47e8d
mlooser <mlooser@gmail.com>
2019-03-15 18:04:04 +0100

    Add recipe preview. Add more date on bootstrap.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,12 @@
-INSERT INTO category (name) VALUES ('dinner');
-INSERT INTO category (name) VALUES ('grill');
-INSERT INTO category (name) VALUES ('quick and easy');
-INSERT INTO category (name) VALUES ('chicken');
-INSERT INTO category (name) VALUES ('mexican');
-INSERT INTO category (name) VALUES ('dip');
-INSERT INTO category (name) VALUES ('vegan');
-INSERT INTO category (name) VALUES ('avocado');
-
+INSERT INTO category (name) VALUES ('American');
+INSERT INTO category (name) VALUES ('Italian');
+INSERT INTO category (name) VALUES ('Mexican');
+INSERT INTO category (name) VALUES ('Fast Food');
 INSERT INTO unit_of_measure (name) VALUES ('Teaspoon');
 INSERT INTO unit_of_measure (name) VALUES ('Tablespoon');
 INSERT INTO unit_of_measure (name) VALUES ('Cup');
 INSERT INTO unit_of_measure (name) VALUES ('Pinch');
 INSERT INTO unit_of_measure (name) VALUES ('Ounce');
+INSERT INTO unit_of_measure (name) VALUES ('Each');
+INSERT INTO unit_of_measure (name) VALUES ('Dash');
+INSERT INTO unit_of_measure (name) VALUES ('Pint');
